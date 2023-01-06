e185afe251ce88a0ef2e840c52959f6f281ec10d
GBDM97 <ghian.m@gmail.com>
2023-01-05 21:02:26 -0300

    init commit

---
+++ src/main/java/com/br/misslegame/MissleGameApplication.java
@@ -0,0 +1,13 @@
+package com.br.misslegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissleGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissleGameApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/br/misslegame/controllers/c1.java
@@ -0,0 +1,13 @@
+package com.br.misslegame.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class c1 {
+    @RequestMapping("api/v1/")
+    public ResponseEntity c1 (){
+
+    }
+
+}
