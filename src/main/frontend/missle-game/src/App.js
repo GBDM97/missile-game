55553c926f024983e542e6d341762008b338cdd5
GBDM97 <ghian.m@gmail.com>
2023-01-14 03:06:02 -0300

    backend is funtional, working in the frontend now

--- src/main/frontend/missle-game/src/App.js
+++ src/main/frontend/missle-game/src/App.js
@@ -1,34 +1,91 @@
 import './App.css';
 import React from 'react';
 import axios from 'axios';
-
+import { useEffect } from 'react';
 
 
 function App() {
-  const [current, set] = React.useState([]);
-  const [currentSt, setSt] = React.useState({backgroundColor: 'transparent', position:'absolute', top:'100px', right: '1330px'});
-  async function fetchData() {
-    axios.get('http://localhost:8080/api/employees')
-    .then((res) => {set(res.data)});
+  const [mouseX, setMouseX] = React.useState([]);
+  const [mouseY, setMouseY] = React.useState([]);
+  const [initX, setInitX] = React.useState([]);
+  const [initY, setInitY] = React.useState([]);
+  const [targetX, setTargetX] = React.useState([]);
+  const [targetY, setTargetY] = React.useState([]);
+  const [currentX, setCurrentX] = React.useState();
+  const [currentY, setCurrentY] = React.useState();
+  const [currentStyle, setStyle] = React.useState();
+  const [currentStyle2, setStyle2] = React.useState();
+  const [currentC, setC] = React.useState(1);
+  const [currentId, setId] = React.useState();
+  const [currentStyle3, setStyle3] = React.useState({width: "50px", height: "2px", backgroundColor: "transparent", position: "absolute"});
+  const [currentBlow, setBlow] = React.useState(false);
+
+  async function createMissle() {
+    var body = {initX:initX, inity:initY, targetX:targetX, targetY:targetY};
+    axios.post('http://localhost:8080/api/v1/createOrLocate', body)
+    .then((res) => {setId(res.data)});
+
+    for (; currentBlow === false;) {
+      var body2 = {id:currentId, initX:initX, inity:initY, targetX:targetX, targetY:targetY};
+      axios.post('http://localhost:8080/api/v1/createOrLocate', body2)
+      .then((res) => {
+        setCurrentX(res.data.currentX);
+        setCurrentY(res.data.currentY);
+        setBlow(res.data.blowUp)
+        setStyle3({
+          width: "50px",
+          height: "2px",
+          backgroundColor: "transparent",
+          position: "absolute",
+          top: currentX,
+          left: currentY,
+        });
+      });
+    }
   }
-fetchData();
-  let i = 1330
+  
   const sendMissle = () => {
-      
-      let px = "px";
-      let right = i + px;
-      setSt({backgroundColor: 'transparent', position:'absolute', top:'100px', right: right })
-      i = i - 1;
-      if (i > 0){setTimeout(sendMissle, 100);}
+      if (currentC === 1) {
+        setStyle({height: "3px",
+          width: "3px",
+          backgroundColor: "white",
+          position: "absolute",
+          top: mouseY,
+          left: mouseX});
+        setC(2);
+        setInitX(mouseX);
+        setInitY(mouseY);
+
+      }else{}
+      if (currentC === 2){
+        setStyle2({height: "3px",
+        width: "3px",
+        backgroundColor: "white",
+        position: "absolute",
+        top: mouseY,
+        left: mouseX})
+        setTargetX(mouseX);
+        setTargetY(mouseY);
+        createMissle();
+      }else{}
   }
     
-    
+    useEffect(() => {
+    const handleMouseMove = (event) => {
+      setMouseX(event.clientX);
+      setMouseY(event.clientY);
+      
+    };
+
+    window.addEventListener('mousemove', handleMouseMove)})
 
   return (
     <>
-        <button style={{color: "white", cursor: "pointer"}} onClick={sendMissle}>SEND</button>
-        <div className='mainView'>
-        <img src={require("./media/missle1.png")} alt="missle1" style={currentSt}/>
+        {/* <button style={{color: "white", cursor: "pointer"}} onClick={sendMissle}>SEND</button> */}
+        <div className='mainView' onClick={sendMissle}>
+        <div style={currentStyle}></div>
+        <div style={currentStyle2}></div>
+        <img src={require("./media/missle1.png")} alt="missle1" style={currentStyle3}/>
         </div>
     </>
   );
